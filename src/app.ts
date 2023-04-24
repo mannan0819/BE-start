223be52193a61071458212990f461e1511d4143f
mannan0819 <mannan1994@hotmail.com>
2023-04-24 00:13:48 -0400

    process env

--- src/app.ts
+++ src/app.ts
@@ -80,4 +80,4 @@ app.use(async (ctx: Context, next: Next) => {
 app.use(userRouter.routes());
 app.use(userRouter.allowedMethods());
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT, () => console.log("Server is running on port 3000"));
