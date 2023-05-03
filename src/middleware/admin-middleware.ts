0e85165e4bb0a8fdeb1e0e31bfe98e37b84a45af
mannan0819 <mannan1994@hotmail.com>
2023-05-03 13:43:08 -0400

    admin middleware

---
+++ src/middleware/admin-middleware.ts
@@ -0,0 +1,16 @@
+import { Context, Next } from "koa";
+import { validateAndReturnUSER } from "../helpers/jwt";
+
+/**
+ * Sets a userId and clientId for use in routes and next middlewares.
+ */
+export default async (ctx: Context, next: Next) => {
+  const userFromToken = await validateAndReturnUSER(ctx);
+  if (userFromToken) {
+    ctx.state.userState = userFromToken;
+    await next();
+  }
+  else {
+    ctx.status = 401;
+  }
+};
